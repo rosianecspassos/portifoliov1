12415e186384cc16f8bcb169533e56426d1d3c35
Rosiane C S dos Passos <rosianecristinasi2013@gmail.com>
2025-04-18 17:02:05 -0300

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -1,27 +1,25 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
+  //Rotas para home   
+Route::prefix('home')->group(function(){
+//Rota para página inicial
+ Route::get('/',[HomeController::class, 'index'])->name('application.index');
+//Dados salvos
+Route::post('/', [HomeController::class, 'store'])->name('application.index'); 
+Route::post('/upload/foto-perfil', [HomeController::class, 'uploadFotoPerfil'])->name('upload.foto-perfil');
 
-Route::get('/home', function () {
-    return view('application.index');
 });
 
-Route::get('/cadastroprincipal', function () {
-    return view('appcadastros.principal');
-});
+  //Rotas para cadastros
+  Route::prefix('cadastros')->group(function(){
+    //Rota para página de cadastro dos dados
+    Route::get('/principal', [HomeController::class, 'create'])->name('cadastros.dadosprincipais'); 
+    Route::get('/principal/update', [HomeController::class, 'update'])->name('applicationindex.blade.php');
 
-Route::get('/cadastroprojetos', function () {
-    return view('appcadastros.addprojetos');
-});
-
-Route::get('/cadastrocursos', function () {
-    return view('appcadastros.addcursos');
-});
-
-Route::get('/cadastrocontato', function () {
-    return view('appcadastros.contatos');
-});
+    });
 
 
 
@@ -34,5 +32,11 @@ Route::middleware('auth')->group(function () {
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
+
+//./vendor/bin/sail mysql -u root
+//Show tables; 
+//DELETE FROM nome_da_tabela;
+//DESC tabela;
+//SELECT * FROM principais;
 
 require __DIR__.'/auth.php';
